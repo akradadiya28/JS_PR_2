2daadd522bbe51bcd0bf64fc345b67980b6137c6
akradadiya28 <akradadiya28@gmail.com>
2024-05-08 20:25:51 +0530

    task-2

---
+++ task-2/js/script.js
@@ -0,0 +1,26 @@
+let month = 5;
+
+let msg = document.getElementById("msg");
+switch (month) {
+    case 11:
+    case 12:
+    case 1:
+    case 2:
+        msg.innerHTML = ("Winter...")
+        break;
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+        msg.innerHTML = ("Summer...")
+        break;
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+        msg.innerHTML = ("Monsoon...")
+        break;
+    default:
+        msg.innerHTML = ("Invalid Month...")
+        break;
+}
