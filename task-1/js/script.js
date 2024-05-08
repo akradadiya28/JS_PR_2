0c4952fe41e3147568c6a97824984d1632dddd11
akradadiya28 <akradadiya28@gmail.com>
2024-05-08 21:01:51 +0530

    task-1

---
+++ task-1/js/script.js
@@ -0,0 +1,18 @@
+let unit = 300;
+
+let amt, totalAmt, surCharge;
+
+if (unit <= 50) {
+    amt = unit * 1;
+} else if (unit <= 150) {
+    amt = 25 + ((unit - 50) * 2);
+} else if (unit <= 250) {
+    amt = 50 + ((unit - 150) * 3)
+} else if (unit > 250) {
+    amt = 220 + ((unit - 250) * 4)
+}
+
+surCharge = amt * 0.20;
+totalAmt = amt + surCharge;
+
+let elctricityBill = document.getElementById("bill").innerHTML = totalAmt;
