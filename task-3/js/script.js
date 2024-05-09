dcfefeab62904f75a00d9c4a257bdd8680c7bbc7
akradadiya28 <akradadiya28@gmail.com>
2024-05-09 08:33:26 +0530

    task-3

---
+++ task-3/js/script.js
@@ -0,0 +1,18 @@
+let P = 20000;
+let R = 8;
+let N = 5;
+
+let intrest = (P * R * N) / 100;
+
+// document.getElementById("int").innerHTML = intrest;
+
+if (N > 3) {
+    if (N <= 5) {
+        R = 3;
+    } else {
+
+    }
+
+} else {
+
+}
